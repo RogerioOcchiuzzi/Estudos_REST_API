d6feaadf8bbef85991aae453003ed357164b01c5
Rogério <rogerio.occhiuzzi@gmail.com>
2020-01-03 11:13:28 -0300

    commit

---
+++ bank_api/client/script.js
@@ -0,0 +1,10 @@
+
+
+function operation(value){
+    
+    //document.getElementById("page").innerHTML = value;
+    document.getElementById("page").innerHTML = '<object type="text/html" data="'+value+'.html"></object>';
+}
+
+//<button class="button_operation" type="button" onclick="operation()">Select Operetion</button>
+//var operationValue = document.getElementById("operation").innerHTML;  
